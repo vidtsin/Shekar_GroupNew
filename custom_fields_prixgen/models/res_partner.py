967d116326d870e0252b3918e17173aff3b6fad8
Prixgen <adarsh@prixgen.com>
2019-08-14 17:43:33 +0530

    New

--- custom_fields_prixgen/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models,_
-
-
-class ResPartner(models.Model):
-	_inherit="res.partner"
-	gst_reg_type = fields.Selection([('registered','Registered'),('unregistered','Unregistered'),('composite','Composite')], default='registered',string="GST Registration Type")
-	
-
-	
