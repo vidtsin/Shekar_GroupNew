e43e81fd530ee4f3aec54d04b8b7bb38f5c0749e
Prixgen <adarsh@prixgen.com>
2019-09-06 16:28:14 +0530

    test

---
+++ shekar_agencies_invoices/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Shekar agencies invoice Model',
+    'version': '12.7.2',
+    'category': 'Tools',
+    'summary': "This module consists, the customized Templates",
+    'depends': ['account_tax_python','account','l10n_in','custom_fields_shekar','customer_vendor_product_assets_number'],
+    'website': 'http://www.prixgen.com',
+    'data': [
+             'views/report_invoice_document_inherit.xml',
+             'views/tax_amount.xml',
+             ],
+    'auto_install': False,
+    'application': True,
+}
